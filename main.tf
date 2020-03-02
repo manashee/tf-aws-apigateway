4945674b334703ccafa563204e21a60d1de9d542
manashee <manashee@gmail.com>
2020-03-02 13:38:21 +0530

    swagger file parametrized

--- main.tf
+++ main.tf
@@ -1,22 +1,11 @@
-#provider "aws" {
-#  access_key = ""
-#  secret_key = ""
-#  profile                           =   "default"
-#  region                            =   "${var.aws_region}"
-#}
-
 provider "aws" {
-  region                  =  "ap-south-1" 
   shared_credentials_file = "/Users/ashokkumar/.aws/credentials"
-  profile                 = "default"
+  profile                           =   "default"
+  region                            =   "${var.aws_region}" 
 }
-
-#module "api-gateway" {
-#  source  = "kurron/api-gateway/aws"
-#  version = "0.9.1"
-#  # insert the 6 required variables here
-#}
 
 module "api_gateway"{
     source  = "./modules/api_gateway"
+    api_name = "${var.api_name}"
+    swgfile = "${file(var.swgfile)}"
 }
